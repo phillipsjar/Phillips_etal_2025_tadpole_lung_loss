dae17ed14b03c03950f46c39a39bda7f64b701f5
phillipsjar <jack.phillips12314@gmail.com>
2023-10-06 14:53:47 -0600

    edits

--- scripts/functions/Bayestraits_simmap.R
+++ scripts/functions/Bayestraits_simmap.R
@@ -5,22 +5,22 @@
 # using the BT posterior to generate Q_matrices while incorporating uncertainty
 
 #              Draw rate matrices from 100 different runs of posterior
-BayesTraits_simmap = function(BT_output, Nsim, data, model, trees, state_labels, trait_col_names){
+BayesTraits_simmap = function(BT_output, Nruns, Nsim, data, model, trees, state_labels, trait_col_names){
   require(phytools)
 if(class(trees) == "phylo"){
   stop("currently implemented for Bayestraits runs that use multiple trees")
 }
   
-random_runs <- sample(1:(dim(BT_output)[1]), Nsim, replace = FALSE)  # get a sample of 100 random runs from the posterior
+random_runs <- sample(1:(dim(BT_output)[1]), Nruns, replace = FALSE)  # get a sample of 100 random runs from the posterior
 tree_numbers <- BT_output$Tree.No[random_runs]
 
-rate_values <- vector(mode = "list", length = Nsim)             # empty list for the rate values for each random run
+rate_values <- vector(mode = "list", length = Nruns)             # empty list for the rate values for each random run
 
 if(model == "dependent" & length(which(colnames(BT_output) == "q12")) == 0){
   stop("dependent (default) model chosen, but q12 rate not available")}
 
 if(model == "dependent"){
-for(i in 0:(Nsim-1)){
+for(i in 0:(Nruns-1)){
   start <- which(colnames(BT_output) == "q12")
   run <- random_runs[i+1]
   rate_values[i+1] <- list(BT_output[(run),start:(start+7)])
@@ -28,7 +28,7 @@ for(i in 0:(Nsim-1)){
 }
 
 if(model == "independent"){
-  for(i in 0:(Nsim-1)){
+  for(i in 0:(Nruns-1)){
     start <- which(colnames(BT_output) == "alpha1")
     run <- random_runs[i+1]
     rate_values[i+1] <- list(BT_output[(run),start:(start+3)])
@@ -36,18 +36,16 @@ if(model == "independent"){
 }
 
 if(model == "six_state"){
-  for(i in 0:(Nsim-1)){
+  for(i in 0:(Nruns-1)){
     start <- which(colnames(BT_output) == "q12")
     run <- random_runs[i+1]
     rate_values[i+1] <- list(BT_output[(run),start:(start+29)])
   }
 }
 
-
-
 source("lung_loss_git/scripts/functions/custom_Q.R")
-rate_values[[1]]
-custom_Q(rate_values[[1]], state_labels, model)
+#rate_values[[1]]
+#custom_Q(rate_values[[1]], state_labels, model)
 
 source("lung_loss_git/scripts/functions/make_simmap_data_function.R")
 
@@ -58,26 +56,30 @@ if(model == "six_state"){mode = model}
 sim_data <- make_simmap_data(data = data, tree = trees[[1]], mode = mode, state_labels = state_labels, trait_col_names = trait_col_names)
 
 
-master_simmap <- vector(mode = "list", length = Nsim)             # empty list to be filled with the descendents of each node of ML tree
+master_simmap <- vector(mode = "list", length = Nruns*Nsim)             # empty list to be filled with the descendents of each node of ML tree
 class(master_simmap) <- "Multiphylo"
 
 #now fill each index of the simmap object with different Q matrices
 if(model == "dependent" | model == "independent"){
-  for(i in 1:Nsim){
-      master_simmap[[i]] <- make.simmap(trees[[tree_numbers[i]]], sim_data, nsim=1, 
+  for(j in 1:(Nruns*Nsim)){
+    for(i in 1:Nruns){
+      master_simmap[[j]] <- make.simmap(trees[[tree_numbers[i]]], sim_data, nsim = 1,
                                    Q = custom_Q(rate_values[[i]], model = model, 
                                    state_labels = state_labels), pi=rep(.25,4))
-    }
-  print(Nsim-i) # count down for sanity
+    }}
+  print(Nruns-i) # count down for sanity
 }
 
+
+
 if(model == "six_state"){
-  for(i in 1:Nsim){
-      master_simmap[[i]] <- make.simmap(trees[[tree_numbers[i]]], sim_data, 
-                          nsim=1, Q = custom_Q(rate_values[[i]], model = model, 
+  for(j in 1:(Nruns*Nsim)){
+    for(i in 1:Nruns){
+      master_simmap[[j]] <- make.simmap(trees[[tree_numbers[i]]], sim_data, nsim = 1,
+                            Q = custom_Q(rate_values[[i]], model = model, 
                               state_labels = state_labels), pi=rep((1/6),6))}
-    print(Nsim-i) # count down for sanity
-}
+    print(Nruns-i) # count down for sanity
+}}
 
 
 return(master_simmap)
--- scripts/main_analyses/Stochastic_character_mapping.R
+++ scripts/main_analyses/Stochastic_character_mapping.R
@@ -6,6 +6,7 @@ load(file = "bayestraits_exports/master_indep_export.Rdata")
 load(file = "bayestraits_exports/master_six_state_dep_export.Rdata")
 load(file = "bayestraits_exports/master_six_state_indep_export.Rdata")
 Nsim <- 10
+Nruns <- 100
 data <- read.csv(file = "lung_loss_git/processed_data/lung_data/no_endo_lung_data.csv")
 trees <- read.nexus(file = "lung_loss_git/bayestraits_trees_data/trees/tree_set.nex")
 
@@ -18,18 +19,19 @@ six_state_col = c("ecology", "lung", "Spec_lotic")
 set.seed(123)
 
 
-t_indep <- BayesTraits_simmap(master_indep, Nsim, data, model = "independent", 
+{t_indep <- BayesTraits_simmap(master_indep, Nruns, Nsim, data, model = "independent", 
                          trees, state_labels = four_state_state, 
                          trait_col_names = four_state_col)
-t_dep <- BayesTraits_simmap(master_dep, Nsim, data, model = "dependent", 
+t_dep <- BayesTraits_simmap(master_dep, Nruns, Nsim, data, model = "dependent", 
                          trees, state_labels = four_state_state, 
                          trait_col_names = four_state_col)
-t_six_indep <- BayesTraits_simmap(master_six_state_indep_export, Nsim, data, 
+t_six_indep <- BayesTraits_simmap(master_six_state_indep_export, Nruns, Nsim, data, 
                          model = "six_state", trees, state_labels = six_state_state, 
                          trait_col_names = six_state_col)
-t_six_dep <- BayesTraits_simmap(master_six_state_dep_export, Nsim, data, 
+t_six_dep <- BayesTraits_simmap(master_six_state_dep_export, Nruns, Nsim, data, 
                          model = "six_state", trees, state_labels = six_state_state, 
-                         trait_col_names = six_state_col)
+                         trait_col_names = six_state_col)}
+
 
 
 rm(list = c("four_state_col", "four_state_state", "six_state_col", "six_state_state", 
@@ -84,13 +86,30 @@ avg_simmap_counts = function(simmap){
   }
   Y <- do.call(cbind, list)
   Y <- array(Y, dim=c(dim(list[[1]]), length(list)))
-  counts <- apply(Y, c(1,2), mean, na.rm = TRUE)
+  counts <- round(apply(Y, c(1,2), mean, na.rm = TRUE),1)
+  rownames(counts) <- rownames(countSimmap(simmap[[1]])$Tr)
+  colnames(counts) <- colnames(countSimmap(simmap[[1]])$Tr)
+  return(counts)
+}
+
+sd_simmap_counts = function(simmap){
+  require(phytools)
+  N <- length(simmap)
+  list <- vector(mode = "list", length = N)             
+  for(i in 1:N){
+    list[i] <- list(countSimmap(simmap[[i]])$Tr)
+  }
+  Y <- do.call(cbind, list)
+  Y <- array(Y, dim=c(dim(list[[1]]), length(list)))
+  counts <- round(apply(Y, c(1,2), sd, na.rm = TRUE),1)
   rownames(counts) <- rownames(countSimmap(simmap[[1]])$Tr)
   colnames(counts) <- colnames(countSimmap(simmap[[1]])$Tr)
   return(counts)
 }
 
 avg_simmap_counts(t_indep)
+sd_simmap_counts(t_indep)
+
 avg_simmap_counts(t_dep)
 avg_simmap_counts(t_six_indep)
 avg_simmap_counts(t_six_dep)
