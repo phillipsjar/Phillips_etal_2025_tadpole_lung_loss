0424cfd2ec36e2216e1d96215518a303bd98dfcb
phillipsjar <jack.phillips12314@gmail.com>
2023-10-06 13:17:22 -0600

    edits

--- scripts/main_analyses/Stochastic_character_mapping.R
+++ scripts/main_analyses/Stochastic_character_mapping.R
@@ -18,16 +18,16 @@ six_state_col = c("ecology", "lung", "Spec_lotic")
 set.seed(123)
 
 
-t1 <- BayesTraits_simmap(master_dep, Nsim, data, model = "dependent", 
+t_indep <- BayesTraits_simmap(master_indep, Nsim, data, model = "independent", 
                          trees, state_labels = four_state_state, 
                          trait_col_names = four_state_col)
-t2 <- BayesTraits_simmap(master_indep, Nsim, data, model = "independent", 
+t_dep <- BayesTraits_simmap(master_dep, Nsim, data, model = "dependent", 
                          trees, state_labels = four_state_state, 
                          trait_col_names = four_state_col)
-t3 <- BayesTraits_simmap(master_six_state_dep_export, Nsim, data, 
+t_six_indep <- BayesTraits_simmap(master_six_state_indep_export, Nsim, data, 
                          model = "six_state", trees, state_labels = six_state_state, 
                          trait_col_names = six_state_col)
-t4 <- BayesTraits_simmap(master_six_state_indep_export, Nsim, data, 
+t_six_dep <- BayesTraits_simmap(master_six_state_dep_export, Nsim, data, 
                          model = "six_state", trees, state_labels = six_state_state, 
                          trait_col_names = six_state_col)
 
@@ -46,11 +46,83 @@ cols2 <- setNames(c("gold", "purple", "#99CCFF", "blue", "red", "#EBFFE1"),
                   c("X_S_unsp", "X_S_sp", "Lu_S_unsp", "Lu_S_sp", "X_P", "Lu_P"))
 require(phytools)
 #tester of one run to see if colors look good
-plotSimmap(t1[[1]], cols, pts=F, ftype="off", fsize = .2, 
+{plotSimmap(t_indep[[1]], cols, pts=F, ftype="off", fsize = .2, 
            outline = TRUE, type = "fan", add=FALSE, lwd=2, offset=0.5)
-plotSimmap(t2[[1]], cols, pts=F, ftype="off", fsize = .2, 
+title(main="independent model",font.main=3,
+      line=-1)}
+
+{plotSimmap(t_dep[[1]], cols, pts=F, ftype="off", fsize = .2, 
            outline = TRUE, type = "fan", add=FALSE, lwd=2, offset=0.5)
-plotSimmap(t3[[1]], cols2, pts=F, ftype="off", fsize = .2, 
+title(main="dependent model",font.main=3,
+      line=-1)}
+
+{plotSimmap(t_six_indep[[1]], cols2, pts=F, ftype="off", fsize = .2, 
            outline = TRUE, type = "fan", add=FALSE, lwd=2, offset=0.5)
-plotSimmap(t4[[2]], cols2, pts=F, ftype="off", fsize = .2, 
+title(main="six_independent model",font.main=3,
+      line=-1)}
+
+{plotSimmap(t_six_dep[[2]], cols2, pts=F, ftype="off", fsize = .2, 
            outline = TRUE, type = "fan", add=FALSE, lwd=2, offset=0.5)
+title(main="six_dependent model",font.main=3,
+      line=-1)}
+
+
+
+t_indep_count <- countSimmap(t_indep[[3]])
+t_dep_count <- countSimmap(t_dep[[1]])
+t_six_indep_count <- countSimmap(t_six_indep[[1]])
+t_six_dep_count <- countSimmap(t_six_dep[[1]])
+
+
+
+avg_simmap_counts = function(simmap){
+  require(phytools)
+  N <- length(simmap)
+  list <- vector(mode = "list", length = N)             
+  for(i in 1:N){
+    list[i] <- list(countSimmap(simmap[[i]])$Tr)
+  }
+  Y <- do.call(cbind, list)
+  Y <- array(Y, dim=c(dim(list[[1]]), length(list)))
+  counts <- apply(Y, c(1,2), mean, na.rm = TRUE)
+  rownames(counts) <- rownames(countSimmap(simmap[[1]])$Tr)
+  colnames(counts) <- colnames(countSimmap(simmap[[1]])$Tr)
+  return(counts)
+}
+
+avg_simmap_counts(t_indep)
+avg_simmap_counts(t_dep)
+avg_simmap_counts(t_six_indep)
+avg_simmap_counts(t_six_dep)
+
+
+
+
+
+
+
+
+
+
+
+
+#     [,1] [,2]
+#[1,]    4    5
+#[2,]    6    7
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
