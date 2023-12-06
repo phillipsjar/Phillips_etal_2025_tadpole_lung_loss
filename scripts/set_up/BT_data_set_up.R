e767c503e110fedde1fef94b64a5831eb4a98446
phillipsjar <jack.phillips12314@gmail.com>
2023-12-05 18:25:50 -0700

    edits

--- scripts/set_up/BT_data_set_up.R
+++ scripts/set_up/BT_data_set_up.R
@@ -11,10 +11,15 @@ source("lung_loss_git/scripts/set_up/Initial_set_up.R")
 
 require(ape)
 
-data_no_endo <- read.csv("lung_loss_git/processed_data/lung_data/no_endo_lung_data.csv")
+data_aqu <- read.csv("lung_loss_git/processed_data/lung_data/aqu_lung_data.csv")
+aqu_tree <- read.nexus(file = "lung_loss_git/bayestraits_trees_data/trees/maxLH_tree_aqu.nex")
 
-BT_ML_tree <- read.nexus(file = "lung_loss_git/bayestraits_trees_data/trees/maxLH_tree.nex")
-BT_tree_set <- read.nexus(file = "lung_loss_git/bayestraits_trees_data/trees/tree_set.nex")
+data_full <- read.csv("lung_loss_git/processed_data/lung_data/full_lung_data.csv")
+full_tree <- read.nexus(file = "lung_loss_git/bayestraits_trees_data/trees/maxLH_tree_full.nex")
+
+tree_set_aqu <- read.nexus(file = "lung_loss_git/bayestraits_trees_data/trees/tree_set_aqu.nex")
+tree_set_full <- read.nexus(file = "lung_loss_git/bayestraits_trees_data/trees/tree_set_full.nex")
+
 ###########################################################################
 ################## HOW TO Make Bayestraits data matrices ##################
 ###########################################################################
@@ -31,19 +36,25 @@ BT_data_mat = function(data, tree, traits){
   return(mat)}
 
 
-BT_mat <- BT_data_mat(data_no_endo, BT_ML_tree, c("ecology", "lung"))
+data_eight <- data_full[!(is.na(data_full$eight_state)),]
 
-six_state_mat <- BT_data_mat(data_no_endo, BT_ML_tree, "six_state")  #for six state data (including specialized and non-specialized lotic as two different states)
 
-dim(BT_mat) # check that the numbers match set up 
-dim(six_state_mat)
+BT_aqu_mat <- BT_data_mat(data_aqu, aqu_tree, c("ecology", "lung"))
+
+eight_state_mat <- BT_data_mat(data_eight, full_tree, "eight_state")  #for six state data (including specialized and non-specialized lotic as two different states)
+
+table(eight_state_mat[,2])
+
+dim(BT_aqu_mat) # doesn't include terrestrial species 
+dim(eight_state_mat) # doesn't include species missing any ecology or lung info
 
 #export tables in BT-friendly format
-write.table(BT_mat,row.names=F, col.names=F, file = "lung_loss_git/bayestraits_trees_data/data/data_matrix.txt", sep = "\t",quote = FALSE)
-write.table(six_state_mat,row.names=F, col.names=F, file = "lung_loss_git/bayestraits_trees_data/data/6state_data_matrix.txt", sep = "\t",quote = FALSE)
+write.table(BT_aqu_mat,row.names=F, col.names=F, file = "lung_loss_git/bayestraits_trees_data/data/data_matrix.txt", sep = "\t",quote = FALSE)
+write.table(eight_state_mat,row.names=F, col.names=F, file = "lung_loss_git/bayestraits_trees_data/data/8state_data_matrix.txt", sep = "\t",quote = FALSE)
 
 
-rm(list = c("BT_mat", "six_state_mat", "BT_data_mat", "data_no_endo"))
+rm(list = c("eight_state_mat", "BT_aqu_mat", "data_aqu", "data_eight",
+            "BT_data_mat", "data_full"))
 
 #########################################################################
 ## HOW TO FINISH BT COMMAND FILES with tags and nodes ##
@@ -55,45 +66,83 @@ rm(list = c("BT_mat", "six_state_mat", "BT_data_mat", "data_no_endo"))
 require(ape)
 require(phytools)
 
-ML_nodes <- vector(mode = "list", length = BT_ML_tree$Nnode)             # empty list to be filled with the descendents of each node of ML tree
+ML_nodes_aqu <- vector(mode = "list", length = aqu_tree$Nnode)             # empty list to be filled with the descendents of each node of ML tree
+ML_nodes_full <- vector(mode = "list", length = full_tree$Nnode)             # empty list to be filled with the descendents of each node of ML tree
 
 
-for(i in 1:length(ML_nodes)){
-  A <- getDescendants(BT_ML_tree, BT_ML_tree$Nnode + 1 + i)              # descendents of ML_tree node
-  B <-  BT_ML_tree$tip.label[A]                                          # convert to names (alternatively, sort by number to only include tips)
-  ML_nodes[[i]] <- B[!(is.na(B))]                                        # remove NAs, leaving only tips
+for(i in 1:length(ML_nodes_aqu)){
+  A <- getDescendants(aqu_tree, aqu_tree$Nnode + 1 + i)              # descendents of ML_tree node
+  B <-  aqu_tree$tip.label[A]                                          # convert to names (alternatively, sort by number to only include tips)
+  ML_nodes_aqu[[i]] <- B[!(is.na(B))]                                        # remove NAs, leaving only tips
 }
 
-ML_node_check <- rep(NA, BT_ML_tree$Nnode)                               # vector to record whether node is present in tree set as well
+for(i in 1:length(ML_nodes_full)){
+  A <- getDescendants(full_tree, full_tree$Nnode + 1 + i)              # descendents of ML_tree node
+  B <-  full_tree$tip.label[A]                                          # convert to names (alternatively, sort by number to only include tips)
+  ML_nodes_full[[i]] <- B[!(is.na(B))]                                        # remove NAs, leaving only tips
+}
+
+ML_node_check_aqu  <- rep(NA, aqu_tree$Nnode)                               # vector to record whether node is present in tree set as well
+ML_node_check_full <- rep(NA, full_tree$Nnode)                               # vector to record whether node is present in tree set as well
 
 #################################################
 #   Next step takes a couple minutes
 #################################################
 for(i in 1:100){                                                         # iterate across 100 trees
-    for(j in 1:BT_ML_tree$Nnode){                                        # iterate across all nodes of ML tree
-      if(!(is.monophyletic(BT_tree_set[[i]], ML_nodes[[j]]))){           # if a clade in ML_tree is not monophyletic in at least 1 tree in the treeset,
-        ML_node_check[j] <- 1                                            # then mark it as such (can take a while, a lot of comparisons, especially for big trees)
+    for(j in 1:aqu_tree$Nnode){                                        # iterate across all nodes of ML tree
+      if(!(is.monophyletic(tree_set_aqu[[i]], ML_nodes_aqu[[j]]))){           # if a clade in ML_tree is not monophyletic in at least 1 tree in the treeset,
+        ML_node_check_aqu[j] <- 1                                            # then mark it as such (can take a while, a lot of comparisons, especially for big trees)
       }
   }
 }
 
-nodes_to_keep <- BT_ML_tree$Nnode + 1 + which(is.na(ML_node_check))     # change to reflect the way R keeps track of nodes vs. tips (assumes tree is bifurcating)
-save(nodes_to_keep, file = "lung_loss_git/bayestraits_trees_data/data/nodes_to_keep.Rdata")      # save this for later plotting, since it takes a while to calculate
-#load(file = "lung_loss_git/bayestraits_trees_data/data/nodes_to_keep.Rdata")
+#################################################
+#   Next step takes a couple minutes
+#################################################
+for(i in 1:100){                                                         # iterate across 100 trees
+  for(j in 1:full_tree$Nnode){                                        # iterate across all nodes of ML tree
+    if(!(is.monophyletic(tree_set_full[[i]], ML_nodes_full[[j]]))){           # if a clade in ML_tree is not monophyletic in at least 1 tree in the treeset,
+      ML_node_check_full[j] <- 1                                            # then mark it as such (can take a while, a lot of comparisons, especially for big trees)
+    }
+  }
+}
 
-data <- read.csv("lung_loss_git/processed_data/lung_data/no_endo_lung_data.csv")
-treedata <- data[data$Taxa %in% BT_ML_tree$tip.label,]; rm(data)
+nodes_to_keep_aqu <- aqu_tree$Nnode + 1 + which(is.na(ML_node_check_aqu))     # change to reflect the way R keeps track of nodes vs. tips (assumes tree is bifurcating)
+nodes_to_keep_full <- full_tree$Nnode + 1 + which(is.na(ML_node_check_full))     # change to reflect the way R keeps track of nodes vs. tips (assumes tree is bifurcating)
+
+save(nodes_to_keep_aqu, file = "lung_loss_git/bayestraits_trees_data/data/nodes_to_keep_aqu.Rdata")      # save this for later plotting, since it takes a while to calculate
+save(nodes_to_keep_full, file = "lung_loss_git/bayestraits_trees_data/data/nodes_to_keep_full.Rdata")      # save this for later plotting, since it takes a while to calculate
+#load(file = "lung_loss_git/bayestraits_trees_data/data/nodes_to_keep_aqu.Rdata")
+#load(file = "lung_loss_git/bayestraits_trees_data/data/nodes_to_keep_full.Rdata")
+
+aqu_data <- read.csv("lung_loss_git/processed_data/lung_data/aqu_lung_data.csv")
+aqu_data <- aqu_data[aqu_data$Taxa %in% aqu_tree$tip.label,]
 cols <- c("magenta", "blue", "red", "green")
 
-plot(BT_ML_tree, show.tip.label = TRUE, cex = .25, 
-     tip.color = cols[treedata$state[match(BT_ML_tree$tip.label, treedata$Taxa)]], type = "f")
+plot(aqu_tree, show.tip.label = TRUE, cex = .25, 
+     tip.color = cols[aqu_data$state[match(aqu_tree$tip.label, aqu_data$Taxa)]], type = "f")
 
-nodelabels(node = nodes_to_keep, frame = "circle", cex = .2)
+nodelabels(node = nodes_to_keep_aqu, frame = "circle", cex = .2)
 
-rm(list = c("A", "B", "i", "j", "ML_node_check", "ML_nodes", "treedata", "cols"))
+
+full_data <- read.csv("lung_loss_git/processed_data/lung_data/full_lung_data.csv")
+full_data <- full_data[full_data$Taxa %in% full_tree$tip.label,]
+cols2 <- c("gold", "purple", "#99CCFF", "blue", "red", "lightgreen", "#844200", "#D28231")
+
+plot(full_tree, show.tip.label = TRUE, cex = .25, 
+     tip.color = cols2[full_data$eight_state[match(full_tree$tip.label, full_data$Taxa)]], type = "f")
+
+nodelabels(node = nodes_to_keep_full, frame = "circle", cex = .2)
+
+
+rm(list = c("A", "B", "i", "j", "ML_node_check_aqu", "ML_nodes_aqu", "aqu_data", 
+            "ML_node_check_full", "ML_nodes_full", "cols", "cols2", "full_data"))
+
+
+
+
 
 # these functions actually write out the taxa names as needed by bayestraits for tags and nodes
-
 
 namelist <- function(x,nodes) {
   for (i in 1:length(x)){
@@ -114,33 +163,50 @@ fnlist <- function(x) {
 
 # Get list of nodes and total number of tips 
 # Create empty list
-desc_list <- vector(mode = "list", length = length(nodes_to_keep))  #output list of descendents for each node
+desc_list_aqu <- vector(mode = "list", length = length(nodes_to_keep_aqu))  #output list of descendents for each node
+desc_list_full <- vector(mode = "list", length = length(nodes_to_keep_full))  #output list of descendents for each node
 
-for(i in 1:length(nodes_to_keep)){
-  a <- BT_ML_tree$tip.label[getDescendants(BT_ML_tree, nodes_to_keep[i])]   #get descendents for each node 
+for(i in 1:length(nodes_to_keep_aqu)){
+  a <- aqu_tree$tip.label[getDescendants(aqu_tree, nodes_to_keep_aqu[i])]   #get descendents for each node 
   b <- a[which(a != "NA")]                                       #remove nodes
-  desc_list[[i]] <- b                                 #add to output as list
-  names(desc_list)[i] <- paste("AddTag ", nodes_to_keep[i], "tag", sep = "")
+  desc_list_aqu[[i]] <- b                                 #add to output as list
+  names(desc_list_aqu)[i] <- paste("AddTag ", nodes_to_keep_aqu[i], "tag", sep = "")
+}
+
+for(i in 1:length(nodes_to_keep_full)){
+  a <- full_tree$tip.label[getDescendants(full_tree, nodes_to_keep_full[i])]   #get descendents for each node 
+  b <- a[which(a != "NA")]                                       #remove nodes
+  desc_list_full[[i]] <- b                                 #add to output as list
+  names(desc_list_full)[i] <- paste("AddTag ", nodes_to_keep_full[i], "tag", sep = "")
 }
 
 #preview what they look like
-fnlist(desc_list)
-namelist(desc_list,nodes_to_keep)
+fnlist(desc_list_aqu)
+namelist(desc_list_aqu,nodes_to_keep_aqu)
 
 
 ### creates and writes portion of command file into a new file named 4062_node_list.txt
 
-sink("lung_loss_git/bayestraits_trees_data/node_list.txt")
+sink("lung_loss_git/bayestraits_trees_data/aqu_node_list.txt")
 cat("\n","\n")
-fnlist(desc_list)
+fnlist(desc_list_aqu)
 cat("\n","\n")
-namelist(desc_list,nodes_to_keep)
+namelist(desc_list_aqu,nodes_to_keep_aqu)
+cat("\n","run", sep = "")
+sink()
+
+sink("lung_loss_git/bayestraits_trees_data/full_node_list.txt")
+cat("\n","\n")
+fnlist(desc_list_full)
+cat("\n","\n")
+namelist(desc_list_full,nodes_to_keep_full)
 cat("\n","run", sep = "")
 sink()
 
 #clean up
-rm(list = c("a", "b", "i", "desc_list", "fnlist", "namelist", "nodes_to_keep",
-            "BT_ML_tree", "BT_tree_set"))
+rm(list = c("a", "b", "i", "desc_list_aqu", "fnlist", "namelist", "nodes_to_keep_aqu",
+            "aqu_tree", "full_tree", "tree_set_aqu", "tree_set_full", "desc_list_full",
+            "nodes_to_keep_full"))
 
 
 
--- scripts/set_up/Initial_set_up.R
+++ scripts/set_up/Initial_set_up.R
@@ -8,18 +8,21 @@ setwd("/Users/jack/desktop/Research/lunglessness/lung_loss_Phillips_etal_2024")
 # uploading data
 {
   data_original <- read.csv("raw_data/Full_data_matrix.csv")                         #full dataset with all columns
-  keep_cols <- c("Family", "Genus", "Taxa", "Portik", "guild", "ecology", "lung", "Spec_lotic")
+  keep_cols <- c("Family", "Genus", "Taxa", "Portik", "guild", "ecology", "terrestrial", "lung", "Spec_lotic")
   data_original <- data_original[,match(keep_cols, colnames(data_original))] # keep only relevant columns
   
 ####################################################
 
-data_original$six_state <- rep(NA)
-data_original$six_state[which(data_original$ecology == 0 & data_original$lung == 0 & data_original$Spec_lotic == 0)] <- 1
-data_original$six_state[which(data_original$ecology == 0 & data_original$lung == 0 & data_original$Spec_lotic == 1)] <- 2
-data_original$six_state[which(data_original$ecology == 0 & data_original$lung == 1 & data_original$Spec_lotic == 0)] <- 3
-data_original$six_state[which(data_original$ecology == 0 & data_original$lung == 1 & data_original$Spec_lotic == 1)] <- 4
-data_original$six_state[which(data_original$ecology == 1 & data_original$lung == 0)] <- 5
-data_original$six_state[which(data_original$ecology == 1 & data_original$lung == 1)] <- 6
+data_original$eight_state <- rep(NA)
+data_original$eight_state[which(data_original$ecology == 0 & data_original$lung == 0 & data_original$Spec_lotic == 0)] <- 1
+data_original$eight_state[which(data_original$ecology == 0 & data_original$lung == 0 & data_original$Spec_lotic == 1)] <- 2
+data_original$eight_state[which(data_original$ecology == 0 & data_original$lung == 1 & data_original$Spec_lotic == 0)] <- 3
+data_original$eight_state[which(data_original$ecology == 0 & data_original$lung == 1 & data_original$Spec_lotic == 1)] <- 4
+data_original$eight_state[which(data_original$ecology == 1 & data_original$lung == 0)] <- 5
+data_original$eight_state[which(data_original$ecology == 1 & data_original$lung == 1)] <- 6
+data_original$eight_state[which(data_original$terrestrial == 1 & data_original$lung == 0)] <- 7
+data_original$eight_state[which(data_original$terrestrial == 1 & data_original$lung == 1)] <- 8
+
 
 data_original$state <- rep(NA)
 data_original$state[which(data_original$ecology == 0 & data_original$lung == 0)] <- 1
@@ -41,8 +44,8 @@ rm(list = c("data_original", "keep_cols"))}
 dim(data)[1]
 # 513 species
 
-# 47 families represented
-#length(unique(data$Family))
+# 51 families represented
+length(unique(data$Family))
 # taxa distribution within families
 table(data$Family)
 
@@ -74,17 +77,13 @@ for(i in 1:dim(data)[1]){
   if(!(data$Taxa[i] %in% ML_tree$tip.label) & data$Portik[i] %in% ML_tree$tip.label){
     data$tree_names[i] <- data$Portik[i]}}
     
-data[is.na(data$tree_names),c(1,2,3,4)]
+#data[is.na(data$tree_names),c(1,2,3,4)]
 
-ML_tree$tip.label[grep("Plectrohyla", ML_tree$tip.label)]
-
-
-
-#remove 10 endotroph taxa
-data_no_endo <- data[c(which(data$ecology != 2), which(is.na(data$ecology))),]       #remove endotrophs but keep taxa with unknown ecology
-
-#data_no_endo_vis <- data_vis[c(which(data_vis$ecology != 2), which(is.na(data_vis$ecology))),]       #remove endotrophs but keep taxa with unknown ecology
-
+dim(data)
+#dim(data[which(data$terrestrial == 1),c(3,6,7,8)])
+#remove 18 terrestrial taxa
+data_aqu <- data[which(data$terrestrial != 1),]  #remove terrestrial taxa but keep taxa with unknown ecology
+dim(data_aqu)
 
 ##############################################################
 #             Trim trees to those datasets
@@ -104,41 +103,53 @@ new_tree = function(tree, data){
 }
 
 ### Trim trees for different purposes
+
 # Visualization tree of all sampled taxa that can be placed in tree
-Full_sampled_tree <- new_tree(ML_tree, data[data$tree_names %in% ML_tree$tip.label,] )
+Full_tree <- new_tree(ML_tree, data[data$tree_names %in% ML_tree$tip.label,] )
+#lose taxa that can't fit in tree
 
-### Bayestraits trees must only include taxa with binary data (no endotrophs)
+### Bayestraits analysis I - only no terrestrial species for a binary comparison
 
-# BT tree used for ancestral reconstructions (ML tree with no endotrophs)
-BT_ML_tree <- new_tree(ML_tree, data_no_endo[data_no_endo$tree_names %in% ML_tree$tip.label,] )
+aqu_tree <- new_tree(ML_tree, data_aqu[data_aqu$tree_names %in% ML_tree$tip.label,] )
+#no terrestrial taxa and lose taxa that can't be placed in tree
 
-#tree_set_posterior_100[[1]]
 
-# BT tree set of 100 trees (no endotrophs)
-{BT_tree_set_trimmed <- tree_set #dummy tree set to be written over
+# BT analysis I - set of 100 trees (no terrestrial)
+{tree_set_aqu <- tree_set #dummy tree set to be written over
+  for(i in 1:100){
+    tree_set_aqu[[i]] <- new_tree(tree_set[[i]], data_aqu[data_aqu$tree_names %in% tree_set[[i]]$tip.label,])
+    tree_set_aqu[[i]]$node.label <- NULL
+    if((i/10) == round((i/10))){print((100 - i)*.1)}} # counter down to zero as loop finishes
+}
+
+# full tree set of 100 trees (all possible taxa)
+{tree_set_full <- tree_set #dummy tree set to be written over
 for(i in 1:100){
-  BT_tree_set_trimmed[[i]] <- new_tree(tree_set[[i]], data_no_endo[data_no_endo$tree_names %in% tree_set[[i]]$tip.label,])
-  BT_tree_set_trimmed[[i]]$node.label <- NULL
+  tree_set_full[[i]] <- new_tree(tree_set[[i]], data[data$tree_names %in% tree_set[[i]]$tip.label,])
+  tree_set_full[[i]]$node.label <- NULL
   if((i/10) == round((i/10))){print((100 - i)*.1)}} # counter down to zero as loop finishes
 }
 
 #write nexus files to bayestraits folder 
 
-write.nexus(BT_ML_tree, file = "lung_loss_git/bayestraits_trees_data/trees/maxLH_tree.nex", translate = TRUE)
-write.nexus(BT_tree_set_trimmed, file = "lung_loss_git/bayestraits_trees_data/trees/tree_set.nex", translate = TRUE)
+write.nexus(Full_tree, file = "lung_loss_git/bayestraits_trees_data/trees/maxLH_tree_full.nex", translate = TRUE)
+write.nexus(aqu_tree, file = "lung_loss_git/bayestraits_trees_data/trees/maxLH_tree_aqu.nex", translate = TRUE)
+write.nexus(tree_set_full, file = "lung_loss_git/bayestraits_trees_data/trees/tree_set_full.nex", translate = TRUE)
+write.nexus(tree_set_aqu, file = "lung_loss_git/bayestraits_trees_data/trees/tree_set_aqu.nex", translate = TRUE)
 
 #write tree files (with node labels) to another folder for later visualization
-write.tree(Full_sampled_tree, file = "lung_loss_git/trees/edited_trees/All_taxa_vis_tree.tre") #includes endotrophs
-write.tree(BT_ML_tree, file = "lung_loss_git/trees/edited_trees/maxLH_BT_vis_tree.tre")        #removes endotrophs
+write.tree(Full_tree, file = "lung_loss_git/trees/edited_trees/All_taxa_vis_tree.tre") #includes endotrophs
+write.tree(aqu_tree, file = "lung_loss_git/trees/edited_trees/maxLH_BT_vis_tree.tre")        #removes endotrophs
 
 
 
-rm(list = c("tree_set", "ML_tree", "i", "new_tree", "BT_tree_set_trimmed"))
+rm(list = c("tree_set", "ML_tree", "i", "new_tree", 
+            "tree_set_aqu", "tree_set_full", "Full_tree", "aqu_tree"))
 
 write.csv(data, file = "lung_loss_git/processed_data/lung_data/full_lung_data.csv")
-write.csv(data_no_endo, file = "lung_loss_git/processed_data/lung_data/no_endo_lung_data.csv")
+write.csv(data_aqu, file = "lung_loss_git/processed_data/lung_data/aqu_lung_data.csv")
 
-rm(list = c("BT_ML_tree", "data", "data_no_endo", "Full_sampled_tree"))
+rm(list = c("data", "data_aqu"))
 
 
 ####################################################################################################
